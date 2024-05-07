f1c113ff8d4bd247c6e3259048cba7feae410878
ARTHURcantu <cantuario.arthur@gmail.com>
2024-05-06 21:50:44 -0300

    começo do app

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//imports de bibliotecas
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+//imports de funções proprias
+import Home from './screens/Home';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet,  } from "react-native"
+const Home = () => {
+    return(
+        <>
+
+        </>
+    )
+}
+const Styles = StyleSheet.create({
+    containerHome: {
+        flex: 1,
+        backgroundColor: '#F0F0F0F'
+    }
+})
+export default Home
