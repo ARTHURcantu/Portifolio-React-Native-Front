0126558d582a7fa4ee5129bcc055913abf50d97b
ARTHURcantu <cantuario.arthur@gmail.com>
2024-05-17 17:21:01 -0300

    leve mudanças no front

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,9 +4,9 @@ const NavBar = () => {
     return(
         <View style={Styles.ContainerNavBar}>
             <View >
-                <Image 
-                    source={{uri: 'https://fastly.picsum.photos/id/655/50/50.jpg?hmac=h55qWEy106YGCR8G6gCpUktJ8_76PNeMB9hb-e8pD4o'}} 
-                    style={Styles.Avatar} 
+                <Image
+                    source={{uri: 'https://fastly.picsum.photos/id/655/50/50.jpg?hmac=h55qWEy106YGCR8G6gCpUktJ8_76PNeMB9hb-e8pD4o'}}
+                    style={Styles.Avatar}
                 />
             </View>
         </View>
@@ -16,8 +16,8 @@ const NavBar = () => {
 const Styles = StyleSheet.create({
     ContainerNavBar: {
         width: '100%',
-        backgroundColor: '#000',
-        maxHeight: 100,
+        backgroundColor: '#000000',
+        minHeight: 100,
         justifyContent: 'space-around'
     },
     Avatar: {
--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,46 @@
 // import bibliotecas
-import { StyleSheet, View, Text} from "react-native"
+import { StyleSheet, View, Text, ImageBackground, FlatList} from "react-native"
 
 // import funções proprias
 import H1 from "../components/H1"
 import NavBar from "../components/NavBar"
 
+
+const GetImagem = async () => {
+    try{
+        const result = await fetch('http://localhost:437/imagem')//depois mudar para a rota do render
+        const data = await result.json()
+        console.log(data.success)
+        setUsers(data.users)
+    } catch (error){
+        console.log('Error getUsers ' + error.message)
+    }
+}
+
 const Home = () => {
     return(
-        <>
+        <ImageBackground 
+            source={{uri: 'https://img.freepik.com/fotos-gratis/renderizacao-3d-do-astronauta_23-2151128635.jpg?t=st=1715969641~exp=1715973241~hmac=ef276b59a1e6aec097dbff84444b8fda21d96f0b7e082ef051178d9cfc76a435&w=740'}}
+            style={Styles.imagefundo}
+        >
+        <View style={Styles.containerHome}>
             <NavBar />
-            <H1>so testano ele</H1>
-        </>
+            <H1>Imagens</H1>
+            <FlatList
+                style={{width:'70%'}}
+                data={d}
+            />
+        </View>
+        </ImageBackground>
     )
 }
 const Styles = StyleSheet.create({
     containerHome: {
         flex: 1,
-        backgroundColor: '#F0F0F0F'
+        alignItems: 'center'
+    },
+    imagefundo: {
+        flex: 1,
     }
 })
 export default Home
